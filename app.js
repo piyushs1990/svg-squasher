167e3136563b7d2817c233892ef50b0c0c0b7539
unknown <piyushs@PiyushS-PC.is.infostrat.com>
2014-08-24 23:37:23 -0400

    Add app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+﻿var http = require('http');
+var port = 3000;
+var express = require("express");
+var app = express();
+
+app.set("view engine", "vash");
+
+app.use(express.static(__dirname + "/public/"));
+
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+
+var server = http.createServer(app);
+
+server.listen(port);
+console.log("Game running at http://localhost:3000/");
